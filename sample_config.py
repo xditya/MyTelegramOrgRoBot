19091141e7ba86b63435f7f08b8978ed3baa9a9a
xditya <58950863+xditya@users.noreply.github.com>
2020-07-24 06:53:56 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -11,12 +11,12 @@ class Config(object):
     CHUNK_SIZE = 10280
     # MyTelegram.org
     # configurtion required while creating new application
-    APP_TITLE = os.environ.get("APP_TITLE", "usetgbot")
-    APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME", "usetgbot")
-    APP_URL = os.environ.get("APP_URL", "https://telegram.dog/UseTGxBot")
-    APP_DESCRIPTION = os.environ.get("APP_DESCRIPTION", "created using https://telegram.dog/UseTGxBot")
+    APP_TITLE = os.environ.get("APP_TITLE", "telebot_scrapperbot")
+    APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME", "telebot_scrapperbot")
+    APP_URL = os.environ.get("APP_URL", "https://telegram.dog/telebot_scrapperbot")
+    APP_DESCRIPTION = os.environ.get("APP_DESCRIPTION", "created using https://telegram.dog/telebot_scrapperbot")
     #
-    FOOTER_TEXT = os.environ.get("FTEXT", "❤️ @SpEcHlDe")
+    FOOTER_TEXT = os.environ.get("FTEXT", "Modified by xditya, made with ❤️ by @SpEcHlDe")
     # the strings used in the different messages
     # in the bot
     START_TEXT = os.environ.get("START_TEXT", Translation.START_TEXT)
